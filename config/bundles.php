4c5cb88b554d39269b886b90df5b231f13afe7c0
sabrimarzMed <sabri.benmarzouk@med.tn>
2022-08-15 14:18:24 +0200

    fix: added symfony annotations bundle

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Baldinof\RoadRunnerBundle\BaldinofRoadRunnerBundle::class => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
